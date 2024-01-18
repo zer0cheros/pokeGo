bc4cfd4d9945a2fd7285d378da0f2c0beed47c85
zer0cheros <christian.wiss@optimaedu.fi>
2024-01-18 17:54:14 +0200

    test

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -44,7 +44,7 @@ export default function FormModal({pokemon}: {pokemon: Pokemon}) {
           <div className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+        <div className="absolute inset-0 z-10 w-screen overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
